4208014062c7d5078f9cd2c0b5b413df639b2c59
sanjaybajnath <sbajnath6151@bths.edu>
2018-10-02 14:45:34 -0400

    10/2/18

--- src/SBajnathLib.java
+++ src/SBajnathLib.java
@@ -1,5 +1,21 @@
 public class SBajnathLib
 {
     //Methods:
+    public static boolean isPrime (int n)
+    {
+        for (int i = 2; i<n; i++)
+        {
+            if (n%i == 0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+    // primePrinter: Prints the first num primes
+    public static void primePrinter(int num)
+    {
+
+    }
     
 }
