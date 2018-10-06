d433c7aa4c3545755999f7bf5db5ec2b8810cc2b
sanjaybajnath <43279602+sanjaybajnath@users.noreply.github.com>
2018-10-05 20:12:02 -0400

    Update TestFile.java

--- src/TestFile.java
+++ src/TestFile.java
@@ -1,7 +1,19 @@
-public class TestFile
+// Sanjay Bajnath - Period 9
+public class testFile
 {
     public static void main (String [] args)
     {
-
+       SBajnathLib.primePrinter(10);
+       System.out.println(SBajnathLib.sumUpTo(100));
+       System.out.println(SBajnathLib.dateStr("01/08/1999"));
+       System.out.println(SBajnathLib.dateStr("1/8/1999"));
+       System.out.println(SBajnathLib.cutOut("catatonic cat","cat"));
+       System.out.println(SBajnathLib.leastCommonMultiple(5,4,3));
+       System.out.println(SBajnathLib.quadSolver(1,0,4));
+       System.out.println(SBajnathLib.quadSolver(1,4,4));
+       System.out.println(SBajnathLib.quadSolver(1,5,6));
+       System.out.println(SBajnathLib.stringUnion("hello","hola","nihao"));
+       System.out.println(SBajnathLib.vigCipher("attackatdawn","lemon"));
     }
 }
+
